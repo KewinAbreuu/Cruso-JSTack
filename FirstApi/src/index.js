5162d7b2ad621e6e953e2d08c93dae3cbcaaf8a2
kewin_Abreu <87028430+KewinAbreuu@users.noreply.github.com>
2022-06-15 10:45:31 -0300

    [KewinAbreu] - Criando Rotas

--- FirstApi/src/index.js
+++ FirstApi/src/index.js
@@ -1,15 +1,24 @@
 // Faço o require do modulo nativo do node (http)
 const http = require('http');
 
+const routes = require('./routes');
 // Crio o servidor
 const server = http.createServer((request, response)=>{
-    // No head passo o status 200 de sucesso e o content type pra ele saber q é um html que to enviando
-    response.writeHead(200, {'content-type':'text/html'});
-    // Escrevo na tela (o proprio html)
-    response.end('<h1>Hello word!</h1>');
+    
+    const route = routes.find((routeObj)=>(
+        routeObj.endpoint === request.url && routeObj.method === request.method
+    ));
+
+    if (route){
+        route.handler(request, response);
+    }else{
+        response.writeHead(404, {'content-type':'text/html'});
+        response.end(`Cannot ${request.method} ${request.url}`);
+    }
+
 });
 
-// Starto o servidor na porta 3000
 server.listen(3000, ()=>{console.log('🔥 Server Rodando! http://localhost:3000')})
 
-// PRA RODAR O SERVIDOR: node src/index.js (no terminal)
+
+// node src/index.js
